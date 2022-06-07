0961121c5aeed781758493860823f32ee4f0e43f
1711061 <104539673+1711061@users.noreply.github.com>
2022-06-07 09:17:04 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 basic.forever(function () {
     serial.writeLine("" + (sonar.ping(
-    DigitalPin.P0,
-    DigitalPin.P0,
-    PingUnit.MicroSeconds
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showString("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
     )))
 })
